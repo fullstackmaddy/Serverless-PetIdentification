d66c597e50ac48218ef88f9cd7bf51d22b46ac7d
mandardhikari <mandardhikari@gmail.com>
2021-02-03 21:39:57 +0530

    Added code to index page and updated some css elements

---
+++ src/BlazorUI/PetIdentificationUI/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace PetIdentificationUI.Pages
+{
+    public partial class Index: ComponentBase
+    {
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+
+        private async Task NavigateToKnowMore()
+        {
+            await JSRuntime.InvokeAsync<object>("open", "https://devstories.konfhub.com/", "_blank");
+        }
+
+    }
+}
